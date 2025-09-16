eafc3f30b13dcbbc95d3f986da1a43caa69c73e7
Kishor Kumar S <kishor.kumar.s@sap.com>
2025-09-16 11:17:56 +0530

    copy all after installing dependencies separately

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi There!');
+    res.send('<h1>Hi There!!</h1>');
 });
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+    console.log('Server is running on port 8080 !!');
 });
