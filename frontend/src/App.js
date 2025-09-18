08c7db8492e3ea4879d2605f1be68bd7e61883f4
Kishor Kumar S <kishor.kumar.s@sap.com>
2025-09-18 20:00:46 +0530

    ReactJs Production grade app Can work with Image and Container Can also use Docker compose optionally

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h3>Welcome to the Visit Counter App
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          Developed by - Kishor Kumar S
+        </h3>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
